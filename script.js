a2c76fcd9691c0b1d892701a2dacad98977c78ad
Jorge Oregon <35702957+DovoMaster@users.noreply.github.com>
2021-06-18 16:32:35 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -78,14 +78,7 @@ function clearStatusClass(element) {
 
 const questions = [
   {
-    question: 'Minecraft',
-    answers: [
-      { text: 'Yes', correct: true },
-      { text: 'No', correct: false }
-    ]
-  },
-  {
-    question: 'Fortnite',
+    question: 'Age of Empires',
     answers: [
       { text: 'Yes', correct: true },
       { text: 'No', correct: false }
@@ -99,98 +92,7 @@ const questions = [
     ]
   },
   {
-    question: 'Mario Bros',
-    answers: [
-      { text: 'Yes', correct: true },
-      { text: 'No', correct: false }
-    ]
-  },
-  {
-    question: 'Pokemon',
-    answers: [
-      { text: 'Yes', correct: true },
-      { text: 'No', correct: false }
-    ]
-  },
-  {
-    question: 'FIFA',
-    answers: [
-      { text: 'Yes', correct: true },
-      { text: 'No', correct: false }
-    ]
-  },
-  {
-    question: 'Genshin Impact',
-    answers: [
-      { text: 'Yes', correct: true },
-      { text: 'No', correct: false }
-    ]
-  },
-  {
-    question: 'The Legend of Zelda',
-    answers: [
-      { text: 'Yes', correct: true },
-      { text: 'No', correct: false }
-    ]
-  },
-  {
-    question: 'League of Legends',
-    answers: [
-      { text: 'Yes', correct: true },
-      { text: 'No', correct: false }
-    ]
-  },
-  {
-    question: 'Halo',
-    answers: [
-      { text: 'Yes', correct: true },
-      { text: 'No', correct: false }
-    ]
-  },
-  {
-    question: 'Mortal Kombat',
-    answers: [
-      { text: 'Yes', correct: true },
-      { text: 'No', correct: false }
-    ]
-  },
-  {
-    question: 'Call of Duty',
-    answers: [
-      { text: 'Yes', correct: true },
-      { text: 'No', correct: false }
-    ]
-  },
-  {
-    question: 'What is 2 + 2?',
-    answers: [
-      { text: 'Yes', correct: true },
-      { text: 'No', correct: false }
-    ]
-  },
-  {
-    question: 'Counter Strike',
-    answers: [
-      { text: 'Yes', correct: true },
-      { text: 'No', correct: false }
-    ]
-  },
-  {
-    question: 'Overwatch',
-    answers: [
-      { text: 'Yes', correct: true },
-      { text: 'No', correct: false }
-    ]
-  },
-  {
-    question: 'Rocket League',
-    answers: [
-      { text: 'Yes', correct: true },
-      { text: 'No', correct: false }
-    ]
-  },
-  {
-    question: 'Smash Bros	',
+    question: 'Angry Birds',
     answers: [
       { text: 'Yes', correct: true },
       { text: 'No', correct: false }
@@ -204,42 +106,56 @@ const questions = [
     ]
   },
   {
-    question: 'GTA',
+    question: 'Apex Legends',
     answers: [
       { text: 'Yes', correct: true },
       { text: 'No', correct: false }
     ]
   },
   {
-    question: 'Gears of War',
+    question: 'Assassins Creed',
     answers: [
       { text: 'Yes', correct: true },
       { text: 'No', correct: false }
     ]
   },
   {
-    question: 'Tomb raider',
+    question: 'Batman Arkham Series',
     answers: [
       { text: 'Yes', correct: true },
       { text: 'No', correct: false }
     ]
   },
   {
-    question: 'Resident Evil',
+    question: 'Battlefield',
     answers: [
       { text: 'Yes', correct: true },
       { text: 'No', correct: false }
     ]
   },
   {
-    question: 'Tetris',
+    question: 'Bioshock',
     answers: [
       { text: 'Yes', correct: true },
       { text: 'No', correct: false }
     ]
   },
   {
-    question: 'Pacman',
+    question: 'Bomberman',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Borderlands',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Call of Duty',
     answers: [
       { text: 'Yes', correct: true },
       { text: 'No', correct: false }
@@ -253,7 +169,637 @@ const questions = [
     ]
   },
   {
+    question: 'Castlevania',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Clash of Clans',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Clash Royale',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Club Penguin',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Contra',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Counter Strike',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Crash Bandicoot',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Cuphead',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Cyberpunk 2077',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Dark Souls',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Dead by Daylight',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
     question: 'Destiny',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Devil May Cry',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Doom',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Dragon Quest',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Fallout',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Farmville',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'FIFA',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Final Fantasy',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Five Nights at Freddys',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Flappy Bird',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'For Honor',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Fortnite',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Forza Motor',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Free Fire',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Gears of War',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Genshin Impact',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Geometry Dash',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'God of War',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'GTA',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Guitar Hero',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Half life',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Halo',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Hitman',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Injustice',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Just Dance',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'King of Fighters',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Kingdom Hearts',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'League of Legends',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Left 4 Dead',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'LEGO Game',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Mario Bros',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Mario Kart',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Marvel vs Capcom',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Mass Effect',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Megaman',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Metal Gear',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Metal Slug',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Metro',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Metroid',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Minecraft',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Monster Hunter',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Mortal Kombat',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Need for Speed',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Outlast',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Overwatch',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Pacman',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Persona 5',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Piano Tiles',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Pikmin',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Plague Inc',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Plantas vs Zombies',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Pokemon',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Portal',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'PUBG',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Red Dead Redemption',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Resident Evil',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Roblox',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Rocket League',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Silent Hill',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Sims',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Skyrim',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Slenderman',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Smash Bros',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Sonic',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Spider-Man',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Splatoon',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Star Wars Battlefront',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Street Fighter',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Tekken',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Tetris',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'The Last of Us',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'The Legend of Zelda',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'The Walking Dead',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'The Witcher',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Tomb Raider',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Uncharted',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Viva Piñata',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Watchdogs',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'Wii Sports',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+  {
+    question: 'World of Warcraft',
     answers: [
       { text: 'Yes', correct: true },
       { text: 'No', correct: false }
