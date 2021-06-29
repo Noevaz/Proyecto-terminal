b34146a01d5e398aa2e2798ba854509699a6df7f
Jorge Oregon <35702957+DovoMaster@users.noreply.github.com>
2021-06-29 16:03:07 -0500

    Add files via upload

---
+++ src/assets/imagenesnovelty.js
@@ -0,0 +1,29 @@
+import minecraft from "./imagenes/minecraft.jpg";
+import genshin from "./imagenes/genshin.jpg";
+import wow from "./imagenes/wow.jpg";
+import skyrim from "./imagenes/skyrim.jpg";
+import fallout from "./imagenes/fallout.jpg";
+import watchdogs from "./imagenes/watchdogs.jpg";
+import kingdom from "./imagenes/kingdom.jpg";
+import dq from "./imagenes/dq.jpg";
+import borderlands from "./imagenes/borderlands.jpg";
+import guitarhero from "./imagenes/guitarhero.jpg";
+import fifa from "./imagenes/fifa.jpg";
+import spiderman from "./imagenes/spider-man.jpg";
+import batman from "./imagenes/batman.jpg";
+
+export default [
+    minecraft,
+    genshin,
+    wow,
+    skyrim,
+    fallout,
+    watchdogs,
+    kingdom,
+    dq,
+    borderlands,
+    guitarhero,
+    fifa,
+    spiderman,
+    batman
+]
